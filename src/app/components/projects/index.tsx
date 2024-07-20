876ea71209175a48d59c2a7e8fa6850ddddc4402
Alexandre_oliveira <alexandredev.frontend@gmail.com>
2024-07-20 14:10:28 -0300

    update

--- src/app/components/projects/index.tsx
+++ src/app/components/projects/index.tsx
@@ -4,59 +4,49 @@
 import React, {useState} from "react";
 import "./style.css";
 import {FiEye} from "react-icons/fi";
-import {Button, Image, Row} from "antd";
+import {Image} from "antd";
 import Title from "antd/es/typography/Title";
+import model1 from "../projects/assets/model1.png";
+import model2 from "../projects/assets/model2.png";
+import model3 from "../projects/assets/model3.png";
+import model4 from "../projects/assets/model4.png";
+import model5 from "../projects/assets/model5.png";
+import model6 from "../projects/assets/model6.png";
 
 export default function Projects() {
   const [pdfurl, setPdfurl] = useState<string>("");
   const models = [
     {
       key: 1,
+      img: model1,
       pdf: "model1.pdf",
     },
     {
       key: 2,
+      img: model2,
       pdf: "model2.pdf",
     },
     {
       key: 3,
+      img: model3,
       pdf: "model3.pdf",
     },
     {
       key: 4,
+      img: model4,
       pdf: "model4.pdf",
     },
     {
       key: 5,
+      img: model5,
       pdf: "model5.pdf",
     },
     {
       key: 6,
+      img: model6,
       pdf: "model6.pdf",
     },
   ];
-
-  const View = (item: {item: string}) => {
-    if (!item) return <></>;
-    return (
-      <div onClick={() => setPdfurl("")} className="boxpdfpreview">
-        <Button
-          className="closebtn"
-          style={{color: "#fff"}}
-          onClick={() => setPdfurl("")}
-        >
-          X
-        </Button>
-
-        <iframe
-          width={"100%"}
-          height={"100%"}
-          className="pdfPreview"
-          src={item.item}
-        ></iframe>
-      </div>
-    );
-  };
 
   return (
     <section
@@ -71,27 +61,23 @@ export default function Projects() {
       <div className="box-models">
         {models.map(item => {
           return (
-            <span className="box-image-model" key={item.key}>
-              <iframe
-                width={"100%"}
-                height={"100%"}
-                className="imgModel"
-                src={`pdf/${item.pdf}`}
-              />
+            <div className="box-image-model" key={item.key}>
+              <Image className="imgModel" src={item.img.src} />
               <button
                 onClick={() => {
-                  setPdfurl(`pdf/${item.pdf}`);
+                  if (typeof window !== "undefined") {
+                    window.open(`pdf/${item.pdf}`);
+                  }
                 }}
                 title="Ver projeto"
               >
                 ver projeto
                 <FiEye></FiEye>
               </button>
-            </span>
+            </div>
           );
         })}
       </div>
-      {pdfurl && <View item={pdfurl}></View>}
     </section>
   );
 }
